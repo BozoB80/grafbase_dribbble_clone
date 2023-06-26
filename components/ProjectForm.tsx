483c6bee38d102fae059f6f68594ab27eb82fa32
BBakula <bozo.bakula@gmail.com>
2023-06-26 15:30:43 +0200

    commit

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,6 +1,11 @@
 'use client'
 
 import { SessionInterface } from "@/common.types"
+import Image from "next/image"
+import FormField from "./FormField"
+import { categoryFilters } from "@/constants"
+import CustomMenu from "./CustomMenu"
+import { useState } from "react"
 
 type Props = {
   type: string
@@ -8,20 +13,89 @@ type Props = {
 }
 
 const ProjectForm = ({ type, session }: Props) => {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [form, setForm] = useState({
+    title: '',
+    description: '',
+    image: '',
+    liveSiteUrl: '',
+    githubUrl: '',
+    category: '',
+  })
 
-  const image = null
-
+ 
   const handleFormSubmit = () => {
 
+  }
+
+  const handleChangeImage = () => {
+
+  }
+
+  const handleStateChange = (fieldName: string, value: string) => {
+    setForm((prevState) => ({ ...prevState, [fieldName]: value}))
   }
 
   return (
     <form onSubmit={handleFormSubmit} className="flexStart form">
       <div className="flexStart form_image-container">
-        <label htmlFor="poster" className="flexCenter form image-label">
-          {!image && 'Choose a poster for your project'}
+        <label htmlFor="poster" className="flexCenter form_image-label">
+          {!form.image && 'Choose a poster for your project'}
         </label>
+        <input 
+          id="image"
+          type="file"
+          accept="image/*"
+          required={type === 'create'}
+          className="form_image-input"
+          onChange={handleChangeImage}
+        />
+        {form.image && (
+          <Image 
+            src={form?.image}
+            alt="poster"
+            fill
+            className="sm:p-10 object-contain z-20"
+          />
+        )}
+      </div>
 
+      <FormField 
+        title="Title"
+        state={form.title}
+        placeholder="Flexibble"
+        setState={(value) => handleStateChange('title', value)}
+      />
+      <FormField 
+        title="Description"
+        state={form.description}
+        placeholder="Showcase and discover remarkable developer projects"
+        setState={(value) => handleStateChange('description', value)}
+      />
+      <FormField 
+        type="url"
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder="https://bbakula-portfolio.vercel.app"
+        setState={(value) => handleStateChange('liveSiteUrl', value)}
+      />
+      <FormField 
+        type="url"
+        title="Github URL"
+        state={form.githubUrl}
+        placeholder="https://github.com"
+        setState={(value) => handleStateChange('githubUrl', value)}
+      />
+
+      <CustomMenu 
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange('category', value)}
+      />
+
+      <div className="flexStart w-full">
+        <button>Create</button>
       </div>
     </form>
   );
