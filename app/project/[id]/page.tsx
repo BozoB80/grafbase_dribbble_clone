12be0978d243d827eedad267f5a3560a1d6e5339
BBakula <bozo.bakula@gmail.com>
2023-06-27 15:17:45 +0200

    commit

---
+++ app/project/[id]/page.tsx
@@ -0,0 +1,9 @@
+const Project = () => {
+  return (
+    <div>
+      Enter
+    </div>
+  );
+}
+
+export default Project;
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,35 +1,30 @@
-'use client'
-
-import Image from "next/image"
-import { MouseEventHandler } from "react"
+import Image from "next/image";
+import { MouseEventHandler } from "react";
 
 type Props = {
-  title: string
-  type: 'button' | 'submit'
-  leftIcon?: string | null
-  rightIcon?: string | null
-  handleClick?: MouseEventHandler
-  isSubmitting?: boolean
-  bgColor?: string
-  textColor?: string
+    title: string,
+    leftIcon?: string | null,
+    rightIcon?: string | null,
+    handleClick?: MouseEventHandler,
+    submitting?: boolean | false,
+    type?: 'button' | 'submit',
+    bgColor?: string,
+    textColor?: string
 }
 
-const Button = ({ title, type, leftIcon, rightIcon, handleClick, isSubmitting, bgColor, textColor }: Props) => {
-  return (
+const Button = ({ title, leftIcon, rightIcon, handleClick, submitting, type, bgColor, textColor }: Props) => (
     <button
-      type={type || 'button'}
-      disabled={isSubmitting}
-      className={`flexCenter gap-3 px-4 py-3 
-      ${textColor || 'text-white'}
-      ${isSubmitting ? 'bg-black/50' : bgColor || 'bg-primary-purple'} 
-      rounded-xl text-sm font-medium max-md:w-full`}
-      onClick={handleClick}
+        type={type || 'button'}
+        disabled={submitting || false}
+        className={`flexCenter gap-3 px-4 py-3 
+        ${textColor ? textColor : 'text-white'} 
+        ${submitting ? 'bg-black/50' : bgColor ? bgColor : 'bg-primary-purple'} rounded-xl text-sm font-medium max-md:w-full`}
+        onClick={handleClick}
     >
-      {leftIcon && <Image src={leftIcon} alt="lefticon" width={14} height={14} />}
-      {title}
-      {rightIcon && <Image src={rightIcon} alt="righticon" width={14} height={14} />}
+        {leftIcon && <Image src={leftIcon} width={14} height={14} alt="left icon" />}
+        {title}
+        {rightIcon && <Image src={rightIcon} width={14} height={14} alt="right icon" />}
     </button>
-  );
-}
+)
 
 export default Button;
